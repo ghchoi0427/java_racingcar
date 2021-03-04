91aff6ff955d52951534b224fd19946758638c9e
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 15:20:42 +0900

    fix: 메시지 변경

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -2,8 +2,8 @@ package View;
 
 public class OutputView {
 
-    final String MSG_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    final String MSG_INPUT_REPS = "시도할 회수는 몇회인가요?";
+    final String MSG_INPUT_CAR_NAMES = "Please input names of cars.(names are distinguished by comma(.))";
+    final String MSG_INPUT_REPS = "how many repetitions?";
 
     public void MsgInputCars() {
         System.out.println(MSG_INPUT_CAR_NAMES);
