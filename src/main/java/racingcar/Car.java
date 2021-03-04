aaf84d4abed5d038cddff9403f0a47721896aa0e
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 20:55:14 +0900

    feat: getName 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,15 @@
 package racingcar;
 
 public class Car {
+
     private final String name;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
     }
-    // 추가 기능 구현
+    public String getName() {
+        return name;
+    }
 
 }
