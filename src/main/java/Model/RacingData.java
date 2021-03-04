761e092a2c16f210d664b5920b901873af859bd0
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 14:17:42 +0900

    feat: trial, carCount, cars 필드, getter,setter 생성

--- src/main/java/Model/RacingData.java
+++ src/main/java/Model/RacingData.java
@@ -1,4 +1,37 @@
 package Model;
 
+import racingcar.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingData {
+    int trial;
+    int carCount;
+    List<Car> cars = new ArrayList<>();
+
+    public int getTrial() {
+        return trial;
+    }
+
+    public void setTrial(int trial) {
+        this.trial = trial;
+    }
+
+    public int getCarCount() {
+        return carCount;
+    }
+
+    public void setCarCount(int carCount) {
+        this.carCount = carCount;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
 }
