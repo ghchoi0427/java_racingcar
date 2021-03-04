829568bb986fe12f4931dca36a706a290aec42e9
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 14:39:01 +0900

    feat: OutputView 메서드 구현

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,4 +1,15 @@
 package View;
 
 public class OutputView {
+
+    final String MSG_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    final String MSG_INPUT_REPS = "시도할 회수는 몇회인가요?";
+
+    public void MsgInputCars() {
+        System.out.println(MSG_INPUT_CAR_NAMES);
+    }
+
+    public void MsgInputNums() {
+        System.out.println(MSG_INPUT_REPS);
+    }
 }
