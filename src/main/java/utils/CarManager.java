2afa40a997d8ba254f95e216d5865b526ee08f7c
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 14:23:37 +0900

    feat: createCars 메서드 생성

--- src/main/java/utils/CarManager.java
+++ src/main/java/utils/CarManager.java
@@ -1,4 +1,13 @@
 package utils;
 
+import racingcar.Car;
+
 public class CarManager {
+
+    public void createCars(String[] carNames){
+        Car[] cars =  new Car[carNames.length];
+        for(int i=0;i< carNames.length;i++){
+            cars[i]=new Car(carNames[i]);
+        }
+    }
 }
