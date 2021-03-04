79f3ca5f069f711adb6fc3301b361d35f3b51128
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 15:26:52 +0900

    feat: inputReps 메서드 생성

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -1,15 +1,21 @@
 package View;
 
 import utils.CarManager;
+import utils.GameManager;
 import utils.InputValidator;
 
 public class InputView {
     CarManager carManager = new CarManager();
     InputValidator inputValidator = new InputValidator();
+    GameManager gm = new GameManager();
 
     public void inputCars(String cars){
         inputValidator.carNameInputVal(cars);
         String[] carNames = cars.split(",");
         carManager.createCars(carNames);
+    }
+
+    public void inputReps(int reps){
+        gm.rData.setTrial(reps);
     }
 }
