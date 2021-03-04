950ddf2650c74ebfadcc7329b8dd402cbb9c538a
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 14:46:42 +0900

    refactor: progress 맵 생성

--- src/main/java/Model/RacingData.java
+++ src/main/java/Model/RacingData.java
@@ -3,12 +3,23 @@ package Model;
 import racingcar.Car;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RacingData {
     int trial;
     int carCount;
     List<Car> cars = new ArrayList<>();
+    Map<Car,Integer> progress = new HashMap<>();
+
+    public Map<Car, Integer> getProgress() {
+        return progress;
+    }
+
+    public void setProgress(Map<Car, Integer> progress) {
+        this.progress = progress;
+    }
 
     public int getTrial() {
         return trial;
