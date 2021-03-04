47a9d028f4801b430a525867bde20e435b6f94f1
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 14:51:13 +0900

    feat: moveOrStay 메서드 구현

--- src/main/java/utils/CarManager.java
+++ src/main/java/utils/CarManager.java
@@ -6,13 +6,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarManager {
-        GameManager gm = new GameManager();
+    GameManager gm = new GameManager();
 
-    public void createCars(String[] carNames){
-        List<Car> cars =  new ArrayList<>();
-        for(int i=0;i< carNames.length;i++){
+    public void createCars(String[] carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < carNames.length; i++) {
             cars.add(new Car(carNames[i]));
         }
         gm.rData.setCars(cars);
     }
+
+    public void moveCar() {
+
+    }
+
+    public boolean moveOrStay() {
+        int random = RandomUtils.nextInt(0, 9);
+        if (random >= 4) {
+            return true;
+        }
+        return false;
+    }
+
 }
