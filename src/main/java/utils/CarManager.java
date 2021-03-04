d8a5e18106a33886ab9bc5945c1d90f05a04801c
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 20:32:51 +0900

    feat: race 메서드 생성

--- src/main/java/utils/CarManager.java
+++ src/main/java/utils/CarManager.java
@@ -2,25 +2,37 @@ package utils;
 
 import racingcar.Car;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CarManager {
     GameManager gm = new GameManager();
 
     public void createCars(String[] carNames) {
-        Map<Car,Integer> progress = new HashMap<>();
+        Map<Car, Integer> progress = new HashMap<>();
 
         for (int i = 0; i < carNames.length; i++) {
-            progress.put(new Car(carNames[i]),0);
+            progress.put(new Car(carNames[i]), 0);
         }
         gm.rData.setProgress(progress);
     }
 
-    public void moveCar() {
+    public void race() {
+        Map<Car, Integer> progress = gm.rData.getProgress();
+        Set<Car> cars = progress.keySet();
+        for (Car c : cars) {
+            int temp = progress.get(c);
+            progress.put(c, temp + moveCar());
+        }
 
+        System.out.println(progress);
+        gm.rData.setProgress(progress);
+    }
+
+    public int moveCar() {
+        if (moveOrStay()) {
+            return 1;
+        }
+        return 0;
     }
 
     public boolean moveOrStay() {
@@ -30,5 +42,4 @@ public class CarManager {
         }
         return false;
     }
-
 }
