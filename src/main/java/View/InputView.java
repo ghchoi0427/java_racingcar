8b05405a74a0132559bd38930e7e1802b621aae6
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 14:33:17 +0900

    feat: carNameInputVal 메서드 사용

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -1,8 +1,15 @@
 package View;
 
+import utils.CarManager;
+import utils.InputValidator;
+
 public class InputView {
+    CarManager carManager = new CarManager();
+    InputValidator inputValidator = new InputValidator();
 
     public void inputCars(String cars){
+        inputValidator.carNameInputVal(cars);
         String[] carNames = cars.split(",");
+        carManager.createCars(carNames);
     }
 }
