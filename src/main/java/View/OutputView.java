edaf608b431ed55222990a37f9dd010ea378a287
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-04 20:44:23 +0900

    feat: drawProgressBar 메서드 생성

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,9 +1,21 @@
 package View;
 
+import racingcar.Car;
+import utils.GameManager;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 public class OutputView {
 
-    final String MSG_INPUT_CAR_NAMES = "Please input names of cars.(names are distinguished by comma(.))";
+    final String MSG_INPUT_CAR_NAMES = "Please input names of cars.(names are distinguished by comma(,))";
     final String MSG_INPUT_REPS = "how many repetitions?";
+
+    GameManager gm;
+
+    public OutputView(GameManager gm) {
+        this.gm = gm;
+    }
 
     public void MsgInputCars() {
         System.out.println(MSG_INPUT_CAR_NAMES);
@@ -14,6 +26,16 @@ public class OutputView {
     }
 
     public void printRaceResult(){
+        Map<Car,Integer> progress = gm.rData.getProgress();
+        Set<Car> keys = progress.keySet();
 
+    }
+
+    public String drawProgressBar(int distance){
+        String bar = "";
+        for (int i = 0; i < distance; i++) {
+            bar+="-";
+        }
+        return bar;
     }
 }
