2f77334feb1d4c0de6f30ffb3efafdbc7163495b
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-05 22:05:31 +0900

    refactor: getWinner 메서드 수정

--- src/main/java/utils/GameManager.java
+++ src/main/java/utils/GameManager.java
@@ -1,7 +1,33 @@
 package utils;
 
 import Model.RacingData;
+import racingcar.Car;
+
+import java.util.*;
 
 public class GameManager {
     public RacingData rData = new RacingData();
+
+    public List<Car> getWinner() {
+        Map<Car, Integer> map = rData.getProgress();
+        List<Car> winners = new ArrayList<>();
+        int maxRecord = getMaxRecord(map);
+
+        for (Map.Entry<Car, Integer> entry : map.entrySet()) {
+            if (maxRecord == entry.getValue()) {
+                winners.add((Car) entry);
+            }
+        }
+        return winners;
+    }
+
+    public int getMaxRecord(Map<Car, Integer> map) {
+        int max = 0;
+        for (Map.Entry<Car, Integer> entry : map.entrySet()) {
+            if (max < entry.getValue()) {
+                max = entry.getValue();
+            }
+        }
+        return max;
+    }
 }
