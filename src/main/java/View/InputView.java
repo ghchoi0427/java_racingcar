cef5964b2d52e5db6b2efd30ed06460da8577798
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-14 15:25:58 +0900

    refactor: 인스턴스 삭제, static 으로 변경

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -1,26 +1,16 @@
 package View;
 
-import utils.CarManager;
-import utils.GameManager;
 import utils.InputValidator;
 
 public class InputView {
-    private CarManager cm;
-    private InputValidator inputValidator = new InputValidator();
-    private GameManager gm;
 
-    public InputView(CarManager cm, GameManager gm) {
-        this.cm = cm;
-        this.gm = gm;
-    }
-
-    public void inputCars(String cars){
-        inputValidator.carNameInputVal(cars);
+    public static String[] inputCars(String cars){
+        InputValidator.carNameInputVal(cars);
         String[] carNames = cars.split(",");
-        cm.createCars(carNames);
+        return carNames;
     }
 
-    public void inputReps(int reps){
-        gm.rData.setTrial(reps);
+    public static void inputReps(int reps){
+
     }
 }
