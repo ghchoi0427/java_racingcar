0811c8c0572c5e065b0012e79d7aee7de81bfc7b
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-14 15:31:57 +0900

    refactor: static 으로 변경, 불필요한 인스턴스 삭제

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -9,30 +9,26 @@ import java.util.Set;
 
 public class OutputView {
 
-    private final String MSG_INPUT_CAR_NAMES = "Please input names of cars.(names are distinguished by comma(,))";
-    private final String MSG_INPUT_REPS = "how many repetitions?";
-    String MSG_FINAL = "final winner : ";
+    private static final String MSG_INPUT_CAR_NAMES = "Please input names of cars.(names are distinguished by comma(,))";
+    private static final String MSG_INPUT_REPS = "how many repetitions?";
+    private static String MSG_FINAL = "final winner : ";
 
-    GameManager gm;
-
-    public OutputView(GameManager gm) {
-        this.gm = gm;
+    public OutputView() {
     }
 
-    public void MsgInputCars() {
+    public static void MsgInputCars() {
         System.out.println(MSG_INPUT_CAR_NAMES);
     }
 
-    public void MsgInputNums() {
+    public static void MsgInputNums() {
         System.out.println(MSG_INPUT_REPS);
     }
 
-    public void printRaceResult() {
-        Map<Car, Integer> progress = gm.rData.getProgress();
-        Set<Car> carSet = progress.keySet();
+    public void printRaceResult(Map<Car, Integer> CarMap) {
+        Set<Car> carSet = CarMap.keySet();
 
         for (Car c : carSet) {
-            System.out.format("%s : %s", c.getName(), progressBarFactory(progress.get(c))).println();
+            System.out.format("%s : %s", c.getName(), progressBarFactory(CarMap.get(c))).println();
         }
         System.out.println();
     }
@@ -45,10 +41,9 @@ public class OutputView {
         return bar;
     }
 
-    public void finalResult() {
-        List<Car> winnerList = gm.getWinner();
-        for(Car c : winnerList){
-            MSG_FINAL+=String.format("%s ",c.getName());
+    public static void finalResult(List<Car> winnerList) {
+        for (Car c : winnerList) {
+            MSG_FINAL += String.format("%s ", c.getName());
         }
         System.out.println(MSG_FINAL);
     }
