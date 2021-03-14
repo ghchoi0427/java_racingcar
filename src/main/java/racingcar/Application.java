4e4c6730234677262b0a5445e335fdc1a491e813
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-15 01:15:28 +0900

    refactor: 불필요한 Scanner 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,12 +2,9 @@ package racingcar;
 
 import utils.GameManager;
 
-import java.util.*;
-
 public class Application {
 
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
         GameManager.startGame();
     }
