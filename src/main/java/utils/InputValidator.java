5a2de3f06a41be44e853093498361af6b9c4b775
choi <ghchoi0427@gmail.com>
2021-03-17 22:15:08 +0900

    feat: InputValidator 구현.

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -2,11 +2,23 @@ package utils;
 
 public class InputValidator {
 
-    public static void carNameInputVal(String names) throws IllegalArgumentException{
+    private static final String ERROR_ZERO_INPUT = "input number cannot be zero";
+    private static final String ERROR_SHORT_CAR = "car must be 2 at least";
+    private static final String ERROR_BLANK_INPUT = "car must be 2 at least";
 
+    public static void carNameInputVal(String names) throws IllegalArgumentException {
+        if (names.split(",").length == 0) {
+            throw new IllegalArgumentException(ERROR_SHORT_CAR);
+        }
+
+        if (names.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_BLANK_INPUT);
+        }
     }
 
-    public static void repInputVal(int repetition) throws  IllegalArgumentException{
-
+    public static void repInputVal(int repetition) throws IllegalArgumentException {
+        if (repetition <= 0) {
+            throw new IllegalArgumentException(ERROR_ZERO_INPUT);
+        }
     }
 }
